ffa158c8e289affa8b7f52465b3d147d5ef6783b
srmzhk17 <srmzhk17@gmail.com>
2024-11-23 20:55:00 +0300

    Easy-21. Merge Two Sorted Lists

--- src/main/java/leetcode/Easy.java
+++ src/main/java/leetcode/Easy.java
@@ -142,16 +142,38 @@ public class Easy {
         return stack.isEmpty();
     }
 
+    //21. Merge Two Sorted Lists
+     public class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode();
+        ListNode current = head;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = new ListNode(list1.val);
+                list1 = list1.next;
+            }
+            else {
+                current.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        current.next = list1 != null ? list1 : list2;
+
+        return head.next;
+    }
+
+
     // 27. Remove Element
     public static int removeElement(int[] nums, int val) {
-        int k = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i] != val){
-                nums[k] = nums[i];
-                k++;
-            }
-        }
-        return k;
+        return 1;
     }
 
     // 28. Find the Index of the First Occurrence in a String
